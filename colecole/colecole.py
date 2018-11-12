48ec5efc81151130d45b454101fb98417e069b85
Jerahmie Radder <radd0012@umn.edu>
2018-11-12 14:47:58 -0600

    updating python interface

--- colecole/colecole.py
+++ colecole/colecole.py
@@ -4,24 +4,43 @@ Method and variable names follow the official material database:
 http://niremf.ifac.cnr.it/docs/DIELECTRIC/AppendixC.html
 """
 
+import sys
 import numpy as np
-import scipy.constants as sp_consts
+from math import pi
+from scipy.constants import epsilon_0
 
-class ColeCole4Pole(object):
-    def __init__(self, ef=0, sig=0, dels=[], taus=[], alfs=[]):
+class ColeCole(object):
+    def __init__(self, ef=0, sig=0, deltas=[], taus=[], alphas=[], freq=300e6):
         self._ef = ef
         self._sigma = sig
-        self._dels = dels
-        self._taus = taus
-        self._alfs = alfs
-        self._epsilon = None
-        self._frequency = None
+        if isinstance(deltas, list):
+            self._deltas = deltas
+        else:
+            self._deltas = []
+            self._deltas.append(deltas)
+        if isinstance(taus, list):
+            self._taus = taus
+        else:
+            self._taus = []
+            self._taus.append(taus)
 
-    def _epsilon_complex(self):
+        if isinstance(alphas, list):
+            self._alphas = alphas
+        else:
+            self._alphas = []
+            self._alphas.append(alphas)
+
+        self._epsilon = None
+        self._frequency = freq
+
+    def _update_epsilon(self):
         """
         Calculate complex electric permittivity over given frequencies.
         """
-        pass
+        omega = self._frequency*2.0*pi
+        self._epsilon = self._ef + self._sigma/(1j*omega*epsilon_0)
+        for i in range(len(self._deltas)):
+            self._epsilon += self._deltas[i]/(1+(1j*omega*self._taus[i])**(1-self._alphas[i]))
 
     @property
     def ef(self):
@@ -34,25 +53,87 @@ class ColeCole4Pole(object):
         return self._sigma
 
     @sigma.setter
-    def sigma(self, sig):
+    def sigma(self, sigma):
         """Setter for the conductivity."""
         self._sigma = sigma
 
     @property
-    def dels(self):
+    def deltas(self):
         """Return the Static relative permittivities (Deltas)."""
-        return self._dels
-        
+        return self._deltas
+
+    @deltas.setter
+    def deltas(self, dels):
+        """Set the relative permittivities."""
+        if isinstance(dels, list):
+            self._deltas = dels
+        else:
+            self._deltas = []
+            self._deltas.append(dels)
+
     @property
-    def alfs(self):
+    def alphas(self):
         """Return the pole broadness values (alphas)."""
-        return self._alfs
+        return self._alphas
+        
+    @alphas.setter
+    def alphas(self, alphas):
+        """Setter for the broadness values (alphas)."""
+        if isinstance(alphas, list):
+            self._alphas = alphas
+        else:
+            self._alphas = []
+            self._alphas.append(alphas)
 
     @property
     def taus(self):
         """Return the relaxation times (taus)."""
         return self._taus
-    
 
-        
-        
+    @taus.setter
+    def taus(self, taus):
+        """Setter for relaxation times."""
+        if isinstance(taus, list):
+            self._taus = taus
+        else:
+            self._taus = []
+            self._taus.append(taus)
+
+    @property
+    def epsilon(self):
+        """Return the complex permittivity."""
+        if self._epsilon is None:
+            self._update_epsilon()
+        return self._epsilon
+    
+def usage(process):
+    """
+    Display colecole usage and exit.
+    """
+    print("")
+    print("Usage: ", process, " [ef] [sig] [deltas] [taus] [alphas] [freq] ")
+    print("")
+    sys.exit()
+
+def colecole_main(args):
+    """
+    Calculate the complex permittivity from the command line.
+    """
+    if len(args) != 7:
+        usage(args[0])
+    cc1 = ColeCole(args[1], args[2], args[3], args[4], args[5], args[6])
+    print("")
+    print(" Inf. freq. rel. permittivity: ", args[1])
+    print(" Conductivity: ", args[2])
+    print(" Delta(s): ", args[3])
+    print(" Tau(s): ", args[4])
+    print(" Alpha(s): ", args[5])
+    print("")
+    print(self._epsilon)
+
+if __name__ == "__main__":
+    """
+    Run colecole as a command line utility.
+    """
+    args = sys.argv
+    colecole_main(args)
--- colecole/test/test_colecole.py
+++ colecole/test/test_colecole.py
@@ -2,16 +2,17 @@
 Unit tests for colecole.py
 """
 import unittest
+import numpy as np
 import colecole
 
 class TestColeCole(unittest.TestCase):
-    """Tests for ColeCole4Pole class"""
+    """Tests for ColeCole class"""
     @classmethod
     def setUpClass(cls):
         pass
 
     def setUp(self):
-        self.test_cc = colecole.ColeCole4Pole()
+        self.test_cc = colecole.ColeCole()
 
     def test_config(self):
         """
@@ -24,14 +25,102 @@ class TestColeCole(unittest.TestCase):
         Make sure the object initializes properly
         """
         self.assertIsInstance(self.test_cc, object)
-        self.assertIsInstance(self.test_cc, colecole.ColeCole4Pole)
+        self.assertIsInstance(self.test_cc, colecole.ColeCole)
+
+    def test_colecole_init(self):
+        """
+        Test valid colecole inits
+        """
+        cc1 = colecole.ColeCole(1, 2, 3, 4, 5)
+        self.assertEqual(1, cc1.ef)
+        self.assertEqual(2, cc1.sigma)
+        self.assertEqual([3], cc1.deltas)
+        self.assertEqual([4], cc1.taus)
+        self.assertEqual([5], cc1.alphas)
+        cc2 = colecole.ColeCole(8, 9,
+                                [1.1, 2.2, 3.3, 4.4],
+                                [5.5, 6.6, 7.7, 8.8],
+                                [9.9, 1.2, 2.3, 3.4])
+        self.assertEqual(8, cc2.ef)
+        self.assertEqual(9, cc2.sigma)
+        self.assertEqual([1.1, 2.2, 3.3, 4.4], cc2.deltas)
+        self.assertEqual([5.5, 6.6, 7.7, 8.8], cc2.taus)
+        self.assertEqual([9.9, 1.2, 2.3, 3.4], cc2.alphas)
 
     def test_sigma_setter_getter(self):
         """
         Test the conductivity setters and getters.
         """
-        self.assertEqual(1, self.test_cc.sigma())
+        self.assertEqual(0, self.test_cc.sigma)
+        self.test_cc.sigma = 1.1
+        self.assertEqual(1.1, self.test_cc.sigma)
         
+    def test_dels_setter_getter(self):
+        """
+        Test the static relative permittivity (Deltas) setters and getters
+        """
+        self.assertEqual([], self.test_cc.deltas)
+        self.test_cc.deltas = 1
+        self.assertEqual([1], self.test_cc.deltas)
+        self.assertEqual(1, len(self.test_cc.deltas))
+        self.test_cc.deltas = [1.1,2.2,3.3,4.4]
+        self.assertEqual([1.1,2.2,3.3,4.4], self.test_cc.deltas)
+        self.assertEqual(4, len(self.test_cc.deltas))
+        
+    def test_alphas_setter_getter(self):
+        """
+        Test the broadness parameter setters and getters.
+        """
+        self.assertEqual([], self.test_cc.alphas)
+        self.test_cc.alphas = 1
+        self.assertEqual([1], self.test_cc.alphas)
+        new_alphas = [1.2, 2.3, 3.4, 4.5]
+        self.test_cc.alphas = new_alphas
+        self.assertEqual(new_alphas, self.test_cc.alphas)
+        self.assertEqual(4, len(self.test_cc.alphas))
+
+    def test_taus_setter_getter(self):
+        """
+        Test the relaxation time parameter setters and getters.
+        """
+        self.assertEqual([], self.test_cc.taus)
+        self.test_cc.taus = 1
+        self.assertEqual([1], self.test_cc.taus)
+
+    def test_epsilon_calc(self):
+        """
+        Test the calculation of relative permittivity.
+        """
+        # Aorta material at 300 MHz
+        cc1 = colecole.ColeCole(4, 0.25, 40, 8.842e-12, 0.1, 300e6)
+        self.assertTrue(np.isclose(43.819275919-15.962534469j, cc1.epsilon))
+        cc2 = colecole.ColeCole(4.0, 0.25,
+                                 [40.0, 50.0],
+                                 [8.843e-12, 3.183e-9],
+                                 [0.1, 0.1])
+        self.assertTrue(np.isclose(47.0376277802 -24.896267623j,
+                                   cc2.epsilon))
+        cc3 = colecole.ColeCole(4.0, 0.25,
+                                 [40.0, 50.0, 1.0e5],
+                                 [8.843e-12, 3.183e-9, 159.155e-6],
+                                 [0.1, 0.1, 0.2])
+        self.assertTrue(np.isclose(48.3209403398-28.8453668977j,
+                                   cc3.epsilon))
+        cc4 =  colecole.ColeCole(4.0, 0.25,
+                                 [40.0, 50.0, 1.0e5, 1.0e7],
+                                 [8.843e-12, 3.183e-9, 159.155e-6, 1.592e-3],
+                                 [0.1, 0.1, 0.2, 0.0])
+        self.assertTrue(np.isclose(48.3209414503-32.17775682845j,
+                                   cc4.epsilon))
+        
+    def test_colecole_main(self):
+        """
+        Test the main routine (for command line).
+        """
+        args1 = ['colecole.py', '4', '0.25', '40', '8.842e-12', '0.1', '300e6']
+        self.assertEqual("43.819275919-15.962534469j",
+                         colecole.colecole_main(args1))
+
     def tearDown(self):
         pass
 
