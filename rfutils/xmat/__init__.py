55e88f7f19b11c1e90487e27575e303a3779b9d5
Jerahmie <radd0012@umn.edu>
2019-10-23 12:03:08 -0500

    moved cst field reader python classes and tests to cstmod

--- rfutils/xmat/__init__.py
+++ rfutils/xmat/__init__.py
@@ -1,9 +1,3 @@
 """ A collection of electromagnetic field field readers
 """
 
-# Import the required submodules
-
-from .field_reader import FieldReader
-from .field_reader_cst2019 import FieldReaderCST2019
-
-
