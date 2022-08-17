f7fa1a51c971725894ee9df18634445824b1b762
Jerahmie Radder <jerahmie@gmail.com>
2022-08-17 11:38:06 -0500

    refactored remap_smat_at_f to read the file and return a hashmap representing the (i,j) and sp parameters string

--- rfutils/smatrix/src/main.rs
+++ rfutils/smatrix/src/main.rs
@@ -1,8 +1,9 @@
 use std::path::Path;
-use smatrix::smatrix_utils::remap_smat_at_f;
+use smatrix::smatrix_utils::*;
 
 fn main() -> (){
     println!("Hello file reader");
     let path = Path::new("../../test_data/s_params_original.txt");
-    remap_smat_at_f(path);
+    let shm = get_smat_at_f(path).unwrap();
+    println!("{}", shm.get(&(1,1)).unwrap());
 }
--- rfutils/smatrix/src/smatrix_utils.rs
+++ rfutils/smatrix/src/smatrix_utils.rs
@@ -1,15 +1,16 @@
 use std::io::Read;
 use std::path::Path;
 use std::fs::File;
-//use std::io::BufReader;
+use std::collections::HashMap;
 use std::io::{self};
 use regex::Regex;
 
-pub fn remap_smat_at_f(file_path: &Path) -> io::Result<()> {
+pub fn get_smat_at_f(file_path: &Path) -> io::Result<HashMap<(i32,i32),String>> {
     println!("remap_smat_at_f");
     let mut f = File::open(file_path)?;
     let mut buf = String::new();
     let rg = Regex::new(r"^\( ([0-9]+) , ([0-9]+) \): (.*)").unwrap();
+    let mut smat_hm: HashMap<(i32, i32), String> = HashMap::new();
     f.read_to_string(&mut buf)?;
     for line in buf.split('\n'){
         let caps = rg.captures(line);
@@ -22,10 +23,15 @@ pub fn remap_smat_at_f(file_path: &Path) -> io::Result<()> {
                                  .map_or("", |m| m.as_str())
                                  .parse::<i32>().unwrap();
                 println!("{}, {}", i, j);
-                println!("The data: {}", caps.get(3).map_or("", |m| m.as_str()));
+                smat_hm.insert((i,j), caps.get(3).map_or("".to_string(), |m| m.as_str().to_string()));
             },
             None => (),
         }
     }
+    Ok(smat_hm)
+}
+
+
+pub fn remap_smat_at_f() -> io::Result<()> {
     Ok(())
 }
