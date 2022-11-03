8c5944a0e70152fa020e08ac98ae938fa4aa1d9c
Jerahmie Radder <radd0012@umn.edu>
2022-11-02 20:45:13 -0500

    added stubs for numpy data transfer between rust and python

--- rfutils/smatrix/setup.py
+++ rfutils/smatrix/setup.py
@@ -1,4 +1,10 @@
 from setuptools import setup
 from setuptools_rust import Binding, RustExtension
 
-
+setup(
+    name="smatrix-rust",
+    version="0.1",
+        rust_extensions=[RustExtension("smatrix_rust.smatrix_rust", binding=Binding.PyO3)],
+    # rust extensions are not zip safe, just like C-extensions.
+    zip_safe=False,
+)
--- rfutils/smatrix/src/lib.rs
+++ rfutils/smatrix/src/lib.rs
@@ -1,5 +1,34 @@
+use pyo3::prelude::*;
+//use pyo3::wrap_pyfunction;
+use numpy::{IntoPyArray, PyArray1, PyReadonlyArrayDyn};
 pub mod smatrix_utils;
-use smatrix_utils::*;
+//use smatrix_utils::*;
+use std::f64::consts::PI;
+
+
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+#[pyfunction]
+fn deg_to_rad<'py>(py: Python<'py>, x: PyReadonlyArrayDyn<f64>) -> &'py PyArray1<f64> {
+    let array = x.as_array();
+    let result_array = array.into_iter()
+                            .map(|xi| PI/180.0*xi)
+                            .collect::<Vec<_>>();
+
+    result_array.into_pyarray(py)
+}
+
+
+
+#[pymodule]
+fn smatrix_rust(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(deg_to_rad, m)?)?; 
+    Ok(())    
+}
 
 #[cfg(test)]
 mod tests {
