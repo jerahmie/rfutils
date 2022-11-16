546b5bc2cd7250bf31814d85c1b6954a1013f972
Jerahmie Radder <radd0012@umn.edu>
2022-11-16 16:25:42 -0600

    added argument parse options for frequency and optional file path

--- rfutils/visualization/print_s_parameters.py
+++ rfutils/visualization/print_s_parameters.py
@@ -12,15 +12,12 @@ from gui_helpers import openfilegui
 def usage():
     """ Display the usage message.
     """
-    print("")
-    print(" usage: print_s_parameters.py input_file")
-    print("   input_file: touchstone file")
-    print("")
-    print(" Example: ")
-    print(" print_s_parameters.py sparameters.s16 | tee s_params_447MHz.txt -- display and save the 16x16 s-parameters file.")
-    print(" print_s_parameters.py sparameters.s16p  -- display the 16x16 s-parameters to STDOUT")
-    print(" print_s_parameters.py sparameters.s8p > s_params_447MHz.txt  -- save the 8x8 s-parameters to file")
-    print("")
+    return os.linesep + " print_s_parameters.py frequency frequency [--file file]\n" + \
+        " input_file: touchstone file\n\n Example: \n" + \
+        " print_s_parameters.py 447 sparameters.s16 | tee s_params_447MHz.txt -- display and save the 16x16 s-parameters file.\n" + \
+        " print_s_parameters.py 297.2 sparameters.s16p  -- display the 16x16 s-parameters to STDOUT\n" + \
+        " print_s_parameters.py 447.0 sparameters.s8p > s_params_447MHz.txt  -- save the 8x8 s-parameters to file" + \
+        os.linesep + os.linesep
 
 def pretty_print_s_parameters(ntwk, f0=447e6):
     """Pretty print the scattering matrix at frequency.
@@ -37,31 +34,32 @@ def pretty_print_s_parameters(ntwk, f0=447e6):
     for i in range(nn):
         for j in range(nn):
             #pretty_str +=   "      " + "    |   ".join(str(np.abs(ntwk.s[f0_ind, i, j]))) + "\n"
-            #print('(', i+1, ',', j+1 ,'): ','{:6.2f} '.format(to_decibel(np.abs(ntwk.s[f0_ind,i,j]))),
+            #'(', i+1, ',', j+1 ,'): ','{:6.2f} '.format(to_decibel(np.abs(ntwk.s[f0_ind,i,j]))),
             #', {:7.2f}'.format(180.0/np.pi*np.angle(ntwk.s[f0_ind,i,j])))
             print('(', i+1, ',', j+1 ,'): ',
-                    '{:6.2f} '.format(to_decibels(ntwk.s[f0_ind,i,j])),
-                    ', {:7.2f}'.format(to_phase_degrees(ntwk.s[f0_ind,i,j])), ' | ',
-                    '{:6.5f} '.format(np.real(ntwk.s[f0_ind,i,j])), 
-                    ', {:6.5f}'.format(np.imag(ntwk.s[f0_ind,i,j])))
-    #print(pretty_str)
+                '{:6.2f} '.format(to_decibels(ntwk.s[f0_ind,i,j])),
+                ', {:7.2f}'.format(to_phase_degrees(ntwk.s[f0_ind,i,j])), ' | ',
+                '{:6.5f} '.format(np.real(ntwk.s[f0_ind,i,j])), 
+                ', {:6.5f}'.format(np.imag(ntwk.s[f0_ind,i,j])))
+    #pretty_str)
 
 
 if __name__ == "__main__":
-    #ts_file = os.path.abspath(os.path.join(os.path.dirname(__file__),
-    #                          '..', '..', 'test_data',
-    #                          '8CH_ELdipole_commongnd-23cmCONE_notraps_cloneELDipole_6-30-2020.s8p')) 
-    #ts_file = os.path.join('D:\\', 'Temp_CST',
-    #                       '8CH-ELD_KU32insert_Lightbulb_RXtrap-tuned_09-28-20.s8p')
     parser = argparse.ArgumentParser(
         prog = 'print_s_parameters',
-        description = 'Utility to print the s-parameters at given frequency.'
+        description = 'Utility to print the s-parameters at given frequency.',
+        usage = usage(),
         epilog = '')
-    
-    try:
-        ts_file
-    except NameError:
-        ts_file = openfilegui(title="Select Touchstone File")
+    parser.add_argument('frequency', help='Frequency (MHz)')
+    parser.add_argument('--file', help='Input Touchstone file', required=False)
+    args = parser.parse_args()
+    #"args: ", args)
+    f0 = float(args.frequency)*1.0e6
+    print("ts_file: ", args.file)
+    ts_file = args.file 
+    # todo: make tsfile optional in argparse and try gui if ts_file is None
+    if ts_file is None: 
+        ts_file = openfilegui(title="Select T.ouchstone File")
         
     print("Touchstone file: ", ts_file)
     print("pretty print s-parameters.")
